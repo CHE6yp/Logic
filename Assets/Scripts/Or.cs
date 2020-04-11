bb5b0497d800e88f46f728f5fcfed85135199ddc
CHE6yp <che6yp@gmail.com>
2020-04-12 00:50:51 +0300

    Levels

---
+++ Assets/Scripts/Or.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[RequireComponent(typeof(LogicalInput))]
+[RequireComponent(typeof(LogicalOutput))]
+public class Or : MonoBehaviour
+{
+    public List<LogicalInput> logicalInputs;
+    public LogicalOutput logicalOutput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (logicalInputs.Any(a => a.value))
+        {
+            logicalOutput.value = true;
+        }
+        else
+        {
+            logicalOutput.value = false;
+        }
+    }
+}
