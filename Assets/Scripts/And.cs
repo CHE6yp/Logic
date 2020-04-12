da20942186fad868bb64a53c4ed4bbe6c200b1de
CHE6yp <che6yp@gmail.com>
2020-04-12 18:35:08 +0300

    TwoInputs class

--- Assets/Scripts/And.cs
+++ Assets/Scripts/And.cs
@@ -3,45 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class And : MonoBehaviour, IOneOutput
+public class And : TwoInputs
 {
-    public List<GameObject> inputElements;
-    public List<LogicalInput> logicalInputs;
-    public LogicalOutput _logicalOutput;
-    public LogicalOutput logicalOutput
+    public override bool Strategy()
     {
-        get { return _logicalOutput; }
-        set { _logicalOutput = value; }
-    }
-
-    private void Awake()
-    {
-        logicalInputs = new List<LogicalInput>();
-        foreach (GameObject ie in inputElements)
-        {
-            LogicalInput l = new LogicalInput();
-            l.source = (ie.GetComponent(typeof(IOneOutput)) as IOneOutput).logicalOutput; //veryBad, but can't expose interface field in inspector otherwise
-            logicalInputs.Add(l);
-        }
-    }
-
-    //TODO уебать апдейт
-    void Update()
-    {
-        //INSANELY BAD
-        foreach (LogicalInput li in logicalInputs)
-        {
-            li.value = (li.source != null) ? li.source.value : false;
-        }
-
-
-        if (!logicalInputs.Any(a => !a.value))
-        {
-            logicalOutput.value = true;
-        }
-        else
-        {
-            logicalOutput.value = false;
-        }
+        return logicalInput1.value && logicalInput2.value;
     }
 }
---
+++ Assets/Scripts/TwoInputs.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TwoInputs : MonoBehaviour, IOneOutput
+{
+    public GameObject inputElement1;
+    public GameObject inputElement2;
+    public LogicalInput logicalInput1;
+    public LogicalInput logicalInput2;
+
+    public LogicalOutput _logicalOutput;
+    public LogicalOutput logicalOutput
+    {
+        get { return _logicalOutput; }
+        set { _logicalOutput = value; }
+    }
+
+    private void Awake()
+    {
+        //veryBad, but can't expose interface field in inspector otherwise
+        if (inputElement1 != null)
+            logicalInput1.source = (inputElement1.GetComponent(typeof(IOneOutput)) as IOneOutput).logicalOutput;
+        if (inputElement2 != null)
+            logicalInput2.source = (inputElement2.GetComponent(typeof(IOneOutput)) as IOneOutput).logicalOutput; 
+
+    }
+
+    //TODO уебать апдейт
+    void Update()
+    {
+        //INSANELY BAD
+        logicalInput1.value = (logicalInput1.source != null) ? logicalInput1.source.value : false;
+        logicalInput2.value = (logicalInput2.source != null) ? logicalInput2.source.value : false;
+
+        if (Strategy())
+        {
+            logicalOutput.value = true;
+        }
+        else
+        {
+            logicalOutput.value = false;
+        }
+    }
+
+    public abstract bool Strategy();
+}
