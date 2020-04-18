120f3376f0f0a679bb4a27562f971bb438cd86ce
CHE6yp <che6yp@gmail.com>
2020-04-18 19:25:15 +0300

    Tried my best with HDRP

--- Assets/Scripts/LogicalElements/Lamp.cs
+++ Assets/Scripts/LogicalElements/Lamp.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
 
 public class Lamp : LogicalElement
 {
     public Light light;
+    public HDAdditionalLightData data;
 
     protected override void Awake()
     {
@@ -18,13 +20,16 @@ public class Lamp : LogicalElement
         logicalOutput.value = v;
         if (v)
         {
-            light.intensity = 1.5f;
-            GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+            //light.intensity = 1.5f;
+            data.intensity = 600f;
+            GetComponent<MeshRenderer>().material.SetFloat("_EMISSIONINTENCITY",3.5f);
         }
         else
         {
-            light.intensity = 0;
-            GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+            //light.intensity = 0;
+            data.intensity = 0;
+            //GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+            GetComponent<MeshRenderer>().material.SetFloat("_EMISSIONINTENCITY", 0);
         }
     }
 }
